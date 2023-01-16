2a395d79cdf6426f50529c97c46a8d9771eeaa8f
Meghan Andrews <mnandrews24@gmail.com>
2023-01-16 10:03:20 -0500

    Adding script for data analysis & cleaning

---
+++ Data_Analysis.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+colors <- read.csv('C:/Users/Mnandrews24/Documents/Data_Viz_Projects/BobRoss_Challenge/BobRossColors/Bob_Ross_Paintings/data/bob_ross_paintings.csv')
+elements <- read.csv('C:/Users/Mnandrews24/Documents/Data_Viz_Projects/BobRoss_Challenge/BobRossColors/data/bob-ross/elements-by-episode.csv')
